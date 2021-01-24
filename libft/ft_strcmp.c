c5a1d4a4b50c1650f5dcf14b5532a56f6b0acb42
sikeda <sikeda@student.42tokyo.jp>
2021-01-24 18:34:12 +0900

    update: libft ft_strcmp

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: sikeda <sikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 17:38:36 by sikeda            #+#    #+#             */
-/*   Updated: 2021/01/24 17:38:37 by sikeda           ###   ########.fr       */
+/*   Updated: 2021/01/24 18:32:14 by sikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	int		i;
-
-	i = 0;
 	while (*s1 || *s2)
 	{
 		if (*s1 != *s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		s1++;
+		s2++;
 	}
 	return (0);
 }
